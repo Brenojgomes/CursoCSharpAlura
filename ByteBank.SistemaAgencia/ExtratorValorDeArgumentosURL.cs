eb0ab12a287521ca12ba812e15f9c8889d42a729
Breno Gomes <breno.jgomes@hotmail.com>
2021-12-15 16:08:17 -0300

    Propriedade Length e sobrecargas de IndexOf

---
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    internal class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if( string.IsNullOrEmpty(url) )
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio. ", nameof(url));
+            }
+
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);  
+        }
+
+        public string GetValor(string nomeParamentro)
+        {
+            return "";
+            //int indiceParametro = _argumentos.IndexOf(nomeParamentro);  
+        }
+
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -13,16 +13,42 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            DateTime dataFimPagamento = new DateTime(2022,8,17);
-            DateTime dataCorrente = DateTime.Now;
 
-            TimeSpan diferenca = dataFimPagamento - dataCorrente;
 
-            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
 
-            Console.WriteLine(dataCorrente);  
-            Console.WriteLine(dataFimPagamento);
-            Console.WriteLine("Vencimento em: " + mensagem);
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+
+
+
+
+            //Testando o IsNullOrEmpty
+            //string textoVazio = "";
+            //string textoNulo = null;
+            //string textoQualquer = "BrenoGomes";
+            //Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            //Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+            //Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
+            
+            
+            // DateTime --> manipulação de tempo (datas,meses,semanas,dias,horas,minutos...)
+            //DateTime dataFimPagamento = new DateTime(2022,8,17);
+            //DateTime dataCorrente = DateTime.Now;
+
+            //TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            //string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            //Console.WriteLine(dataCorrente);  
+            //Console.WriteLine(dataFimPagamento);
+            //Console.WriteLine("Vencimento em: " + mensagem);
+            
             Console.ReadLine(); 
 
         }
