7e6e1aac92ed97a1e6f1b62b12fa4985896223f1
Breno Gomes <breno.jgomes@hotmail.com>
2021-12-15 10:30:17 -0300

    Add Humanizer

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Humanizer;
 
 
 namespace ByteBank.SistemaAgencia
@@ -12,7 +13,19 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);  
+            DateTime dataFimPagamento = new DateTime(2022,8,17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataCorrente);  
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine("Vencimento em: " + mensagem);
+            Console.ReadLine(); 
+
         }
+
     }
 }
